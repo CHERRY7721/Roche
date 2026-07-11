baf0f86180d5fe4b6ca3cb06b7ce048478c0c24b
CHERRY7721 <3099403323@qq.com>
2026-07-12 01:08:35 +0800

    Add classmate plugin configuration file
    Initial creation of the classmate plugin with metadata.

---
+++ 1classmate-plugin.js
@@ -0,0 +1,14 @@
+{
+  "id": "youth-classmate-book",
+  "name": "青春同学录",
+  "version": "1.0.0",
+  "description": "专属同学录，生成、填写、聊天，满满的青春记忆",
+  "author": "你的名字",
+  "entry": "https://raw.githubusercontent.com/CHERRY7721/Roche/main/classmate-plugin.js",
+  "permissions": [
+    "ui",
+    "conversation:read",
+    "storage",
+    "memory:write"
+  ]
+}
